9a9204b3f2d60e31341e1c410a2ad135089210a8
Matheus Chiodi <matheuschiodi20@gmail.com>
2024-02-23 16:27:15 -0300

    add: SplashScreen

--- pinterest/src/app/_layout.tsx
+++ pinterest/src/app/_layout.tsx
@@ -1,32 +1,32 @@
-import { Slot } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import * as SplashScreen from 'expo-splash-screen';
+import { Slot } from "expo-router"
+import { StatusBar } from "react-native"
+import { GestureHandlerRootView } from "react-native-gesture-handler"
+import * as SplashScreen from "expo-splash-screen"
 
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_500Medium,
   Roboto_700Bold,
-} from '@expo-google-fonts/roboto';
+} from "@expo-google-fonts/roboto"
 
-SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync()
 
 export default function Layout() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
     Roboto_500Medium,
     Roboto_700Bold,
-  });
+  })
 
-  if (!fontsLoaded) {
-    SplashScreen.hideAsync();
+  if (fontsLoaded) {
+    SplashScreen.hideAsync()
   }
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <StatusBar style="light" />
-      <Slot />
+      <StatusBar barStyle="light-content" />
+      {fontsLoaded && <Slot />}
     </GestureHandlerRootView>
-  );
+  )
 }
