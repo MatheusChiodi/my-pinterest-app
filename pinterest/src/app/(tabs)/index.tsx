2322d7f1b1a74583e10d2d44dfc6a0a593c47e3a
Matheus Chiodi <matheuschiodi20@gmail.com>
2024-02-23 15:53:36 -0300

    base

---
+++ pinterest/src/app/(tabs)/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { theme } from '../../theme';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: theme.colors.black,
+  },
+  text: { fontSize: 22, color: theme.colors.white },
+});
---
+++ pinterest/src/theme/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  white: '#ffff',
+  black: '#000',
+  gray: {
+    800: '#292929', 
+    700: '#4b4b4b',
+    600: '#8e8e8e',
+  }
+};
